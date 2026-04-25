5f9a3bc6877ea5cf093097c5c48eb33b0d85c6ba
Mukhtor Eshboyev🪄 <aestdile@gmail.com>
2026-04-25 20:14:44 +0500

    Add comments about AI model and its source
    Added comments explaining the AI model and its creation.

--- Birinchi-Loyiha/amaliy-1.py
+++ Birinchi-Loyiha/amaliy-1.py
@@ -1,1 +1,3 @@
+# Rasmlar asosida buyumlarni(ixtiyoriy)taniydigan sun'iy intellekt modeli.
+# Ushbu model Teachable Machine tizimi va Simple mass downloader extensionlari yordamida yaratildi.
 https://teachablemachine.withgoogle.com/models/Tf534vj8m/
